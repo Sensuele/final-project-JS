ee8d9110ee2a55ff8abefc3d0890da32b8c70e38
Евгений Дружняев <druzhniaev@gmail.com>
2019-05-25 20:07:34 +0600

    табы для окон

--- src/js/parts/modal.js
+++ src/js/parts/modal.js
@@ -1,59 +1,23 @@
 // function modal() {
-// 	let modalPopup = document.querySelector(".popup"),
-// 		engPopup = document.querySelector(".popup_engineer"),
-// 		body = document.querySelector("body"),
-// 		overlayPic = document.createElement("div");
+//   let 
+//       popupEngineer = document.querySelector('.popup_engineer'),
+//       popupEngineerBtn = document.querySelector(".popup_engineer_btn"),
+//       close = document.querySelector(".popup_close");
 
-// 	body.appendChild(overlayPic);
-// 	overlayPic.classList.add('bigPicture', 'overlay', 'animated', 'fadeIn');
-
-// 	let overlay = document.querySelectorAll(".overlay");
-
-// 	function showModal(modal) {
-// 		modal.style.display = "block";
-// 		document.body.style.overflow = "hidden";
-// 	}
-// 	setTimeout(function openMod() {
-// 		showModal(modalPopup);
-// 	}, 60000);
-
-// 	function hideModal(modCloseBtn) {
-// 		[...overlay].forEach(element => {
-// 			element.style.display = "none";
-// 		});
-// 		document.body.style.overflow = "";
-
-// 		let statusMessage = document.querySelector('.status');
-// 		if (statusMessage != undefined) {
-// 			statusMessage.innerHTML = '';
-// 		}
-// 	}
-
-// 	function bigPictures(picture) {
-// 		let pic = picture.parentNode.href;
-// 		overlayPic.innerHTML = `<img src='${pic}'>`;
-// 		showModal(overlayPic);
-// 	}
-
-// 	body.addEventListener("click", e => {
-
-// 		let target = e.target;
-// 		if (target && target.classList.contains("header_btn")) {
-// 			e.preventDefault();
-// 			showModal(engPopup);
-// 		}
-// 		if (target && target.classList.contains("phone_link")) {
-// 			e.preventDefault();
-// 			showModal(modalPopup);
-// 		}
-// 		if (target && target.classList.contains("close_btn") || target.classList.contains("overlay")) {
-// 			hideModal(target);
-// 		}
-// 		if (target && target.classList.contains("lupa") || target.classList.contains("small_picture")) {
-// 			e.preventDefault();
-// 			bigPictures(target);
-// 		}
-// 	});
+//   // Модальное окно на кнопку "Вызвать замерщика"
+//   popupEngineerBtn.addEventListener('click', () => {
+//     popupEngineer.style.display = 'block';
+//     document.body.style.overflow = 'hidden';
+//   });
+//   // Закрыть модальное окно
+//   close.addEventListener('click', () => {
+//     popupEngineer.style.display = 'none';
+//     document.body.style.overflow = '';
+//   });
+  
 // }
 
-// export default modal;
+
+ 
+ 
+    module.exports = modal;
