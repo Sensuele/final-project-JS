67bc32a6ba1f5810d87b571a93ce5660bffdfcfb
Евгений Дружняев <druzhniaev@gmail.com>
2019-05-25 22:10:15 +0600

    табы для отделки

--- src/js/parts/modal.js
+++ src/js/parts/modal.js
@@ -20,4 +20,4 @@
 
  
  
-    module.exports = modal;
+    // module.exports = modal;
--- src/js/parts/tabs.js
+++ src/js/parts/tabs.js
@@ -2,7 +2,10 @@ function tabs() {
 
 	let info = document.querySelector(".info"),
       tabContent = document.querySelectorAll(".tabcontent_glazing"),
-      tab = document.querySelectorAll(".glazing_block");
+      tab = document.querySelectorAll(".glazing_block"),
+      decorContent = document.querySelectorAll(".tabcontent_decoration"),
+      tabDecor = document.querySelectorAll(".decoration_tab"),
+      header = document.querySelector(".decoration_header");
       
 
       // табы для блока "ОСТЕКЛЕНИЕ БАЛКОНОВ И ЛОДЖИЙ"
@@ -40,7 +43,40 @@ function tabs() {
     }
   });
 
-	
+  // Табы для блока с отделкой
+  function hideDecorContent(a) {
+    for (let i = a; i < decorContent.length; i++) {
+      decorContent[i].classList.remove("show");
+      decorContent[i].classList.add("hide");
+    }
+    for (let i = a; i < tabDecor.length; i++) {
+      tabDecor[i].classList.remove("after_click");
+      tabDecor[i].classList.add("no_click");
+    }
+  }
+  hideDecorContent(1);
+
+  function showDecorContent(b) {
+    if (decorContent[b].classList.contains("hide")) {
+      decorContent[b].classList.remove("hide");
+      decorContent[b].classList.add("show");
+    }
+    if (tabDecor[b].classList.contains("no_click")) {
+      tabDecor[b].classList.remove("no_click");
+      tabDecor[b].classList.add("after_click");
+    }
+  }
+  header.addEventListener("click", (e) => {
+    let target = e.target;
+      if (target && target.classList.contains("decoration_tab") || target.parentNode.classList.contains("decoration_tab")) {
+        tabDecor.forEach(function (e, i) {
+          if (target == e || target.parentNode == e) {
+            hideDecorContent(0);
+            showDecorContent(i);
+          }
+        });
+      }
+  });
 }
 
 module.exports = tabs;
